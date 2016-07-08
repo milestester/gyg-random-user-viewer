4c2c21b7c7ad6b96e1349f479f7d1ed795ce2b08
Miles Tester <milesdeant@gmail.com>
2016-07-08 13:58:19 -0700

    Basic functionality - displays image, resets after 60 seconds, basic styling

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Get Your Guide Random User Visualization Coding Challenge
 
-To start:
+To start: <br>
 1. npm install <br>
 2. npm start <br>
 3. Navigate to http://localhost:8080/index.html
--- js/app.js
+++ js/app.js
@@ -1,5 +1,39 @@
-angular.module('gygRandomUserViewerApp', [])
+angular.module('gygRandomUserViewerApp', ['ngLoadingSpinner'])
 
-.controller('gygController', function($scope) {
-  $scope.title = "Start Template";
+.controller('viewerController', function($scope, $gygRandomUser, $interval) {
+  $scope.getUserData = function() {
+    $gygRandomUser.getRandomUser().then(
+      function(res) {
+          console.log(res);
+          $scope.name = res.customerFirstName;
+          $scope.filler = "is enjoying";
+          $scope.attraction = res.activityTitle;
+          $scope.image = res.activityPictureUrl;
+      },
+      function(error) {
+        console.log(error);
+      }
+    );
+  };
+  $scope.getUserData();
+  $interval($scope.getUserData, 60000);
 })
+
+.factory('$gygRandomUser', function($http, $q) {
+  return {
+    getRandomUser : function() {
+      return $http.get("https://www.getyourguide.com/touring.json?key=2Gr0p7z96D").then(
+        function(payload) {
+          return payload.data;
+        },
+        function(error) {
+          return $q.reject(error);
+        }
+      );
+    }
+  }
+})
+
+.directive('activityContainer', function() {
+
+});
