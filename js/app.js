81365c169fb1b4966d7d04d00de7d4560e5d47f4
Miles Tester <milesdeant@gmail.com>
2016-07-08 16:48:43 -0700

    add google map to bottom corner

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 angular.module('gygRandomUserViewerApp', ['ngLoadingSpinner'])
 
 .controller('viewerController', function($scope, $gygRandomUser, $interval) {
+  $scope.fillers = ['is enjoying', 'is loving'];
+
+  $scope.setupMap = function() {
+    var lat = $scope.lat;
+    var long = $scope.long;
+    var mapOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(lat, long),
+      disableDefaultUI: true
+    };
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, long),
+      map: $scope.map,
+      title: $scope.attraction
+    });
+  };
+
   $scope.getUserData = function() {
     $gygRandomUser.getRandomUser().then(
       function(res) {
           console.log(res);
           $scope.name = res.customerFirstName;
-          $scope.filler = "is enjoying";
+          $scope.filler = $scope.fillers[Math.floor(Math.random()*$scope.fillers.length)];
           $scope.attraction = res.activityTitle;
           $scope.image = res.activityPictureUrl;
+          $scope.lat = res.activityCoordinateLatitude;
+          $scope.long = res.activityCoordinateLongitude;
+          $scope.setupMap();
       },
       function(error) {
         console.log(error);
@@ -20,16 +41,21 @@ angular.module('gygRandomUserViewerApp', ['ngLoadingSpinner'])
 })
 
 .factory('$gygRandomUser', function($http, $q) {
+  var payloadData = {};
   return {
     getRandomUser : function() {
       return $http.get("https://www.getyourguide.com/touring.json?key=2Gr0p7z96D").then(
         function(payload) {
-          return payload.data;
+          payloadData = payload.data;
+          return payloadData;
         },
         function(error) {
           return $q.reject(error);
         }
       );
+    },
+    getPayloadData : function () {
+      return payloadData;
     }
   }
 })
